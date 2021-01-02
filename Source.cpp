41b2a408278a875746c131c02c03cda75c1ddced
Vasilshift <Vasilshift@github.com>
2021-01-03 00:55:59 +0500

    Add file README.md and entered g = num * num

--- Source.cpp
+++ Source.cpp
@@ -3,28 +3,30 @@ using namespace std;
 
 int main() {
 
-	long long int num, p = 1;
-	int i = 1, a, sum = 0;
+	long long int num, g, sum = 0, p = 1;
+	int i = 1, a;
 	cout << "Please, enter value: ";
 	cin >> num;
 
+	g = num * num;
+	
 	while (p * 10 <= num) {
 		p *= 10;
 	}
-
+	
 	while (p) {
 
 		a = num / p;
 		sum = sum + a;
-		
+
 		num = num % p;
 		p = p / 10;
 
 	}
-	
 
-	cout << sum << " ";
-
+	if (sum * sum * sum == g) {
+		cout << "condition is met! ";
+	}
 
 	return 0;
 }
