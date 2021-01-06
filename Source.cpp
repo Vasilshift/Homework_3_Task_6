5f1b526e1e6b3a74b866e6655683050686b07ce3
Vasilshift <Vasilshift@github.com>
2021-01-06 22:55:48 +0500

    Work

--- Source.cpp
+++ Source.cpp
@@ -23,41 +23,4 @@ int main() {
 	
 	
 		return 0;
-
-				c = (sum * sum * sum);
-			a = a / 10;
-
-			}
-
-
-
-}
-
-	
-
-	
-		
-	cout << c << " " <<  b;
-		
-
-		
-	
-	return 0;
-
-				c = (sum * sum * sum);
-
-		num = num % p;
-		p = p / 10;
-
-	}
-
-	cout << "Sum of digits of value is: " << sum << endl;
-	cout << "Cube of digits of value is: " << sum * sum * sum << endl;
-	cout << "Composition of value * value is: " << g << endl;
-
-	if (sum * sum * sum == g) {
-		cout << "condition is met! ";
-	}
-
-	return 0;
 }
